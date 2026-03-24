f266f36050f29e37526968104032ab83a705d2bc
Sidney <sidney.santos.zx@gmail.com>
2026-03-24 16:27:56 -0300

    feat: service Uptade product

---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,35 @@
+import { productsRepositories } from "@modules/repositories/ProductsRepositories";
+import { Product } from "../database/entities/Products";
+import AppError from "@shared/errors/AppError";
+
+interface IUpdateProduct {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+export default class UpdateProductService {
+  async execute({id, name, price, quantity}: IUpdateProduct): Promise<Product> {
+    const product = await productsRepositories.findById(id) //verifica se o produto existe
+
+    if (!product) {
+       throw new AppError('Product not found', 404)
+    }
+
+    const productExists = await productsRepositories.findByName(name); //verificar se o nome do produto existe ou não
+
+    if(productExists) {
+      throw new AppError('There is already one product with this name', 409);
+    }
+
+    //Atualizações feitas
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await productsRepositories.save(product);
+
+    return product;
+  }
+}
