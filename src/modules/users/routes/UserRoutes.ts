ad46dbf9e898a9c320e2da4bcdc4a9bd6d566967
Sidney <sidney.santos.zx@gmail.com>
2026-04-06 23:36:38 -0300

    feat: auth middleware

--- src/modules/users/routes/UserRoutes.ts
+++ src/modules/users/routes/UserRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import UsersControllers from "../controllers/UsersControllers";
 import { createUserSchema } from "../schemas/UserSchemas";
+import AuthMiddleware from "@shared/middlewares/authMiddleware";
 
 const userRouter = Router();
 const usersController = new UsersControllers();
 
-userRouter.get('/', usersController.index);
+userRouter.get('/', AuthMiddleware.execute, usersController.index);
 userRouter.post('/', createUserSchema, usersController.create);
 
 export default userRouter;
